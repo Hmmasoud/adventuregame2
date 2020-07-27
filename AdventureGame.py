2dc319cc641c493b2d9b4564474cfc2aaa57dac7
Hala Al Masoud <63056957+Hmmasoud@users.noreply.github.com>
2020-07-27 15:24:34 +0300

    Updated the Validation Issue
    I managed to validate the functions of:
    1- Change Your Weapon
    2- Name Entered by Player
    3- Retrying the Game again.

--- AdventureGame.py
+++ AdventureGame.py
@@ -6,13 +6,14 @@ Places = ['Fields of Elysium (1)', 'Torment of Hades (2)', 'Greece (3)']
 Weapons = ['Sword', 'Bow', 'Dagger', 'Hammer', 'Stinger']
 Opponents = ['Ancient Soldiers', 'Kolossi', 'Cerberus', 'Minotaur']
 Game_Ending = ['Succeed', 'Lose']
-
+Options = ['Yes', 'No', 'yes', 'no', 'Y', 'N', 'y', 'n']
 # End Configuration
 
 
-# 1 defining a function to clear the space for invalide entries 
+# 1 defining a function to clear the space for invalide entries
 def cls():
     print('\n'*100)
+
 
 # 2 defining function to start the game
 def start_game():
@@ -34,15 +35,20 @@ def start_game():
             Option = ''
             cls()
 
+
 # 3 defining function t use as print and put time.sleep together
 def PrintSleep(msg, seconds):
     print(msg + '\n')
     time.sleep(seconds)
 
+
 # 4 defning function to change the weapean if needed
 def ChangeYourWeapon():
-    newrequest =input('Do you want to change your current weapon?\n')
-    if newrequest == 'yes':
+    newrequest = input('Do you want to change your current weapon?\n')
+    while newrequest not in Options:
+        print('Please enter a valid response (Yes/Y or No/N.)')
+        newrequest = input('Do you want to change your current weapon?\n')
+    if newrequest == 'yes' or newrequest == 'y':
         newweapon = random.choice(Weapons)
         print('You now have a ' + newweapon)
 
@@ -51,12 +57,11 @@ def ChangeYourWeapon():
 RetryGame = 'Yes'.lower()
 while RetryGame == 'Yes'.lower():
     # Get the player's name
-    PrintSleep('Enter Your Name to start your adventure:', 1)
-    name = input('')
+    name = input('Enter Your Name to start your adventure:\n')
+    while name.strip() == '':
+        name = input('Please enter your name.\n')
     start_game()
     time.sleep(2)
-
-
     weapon = random.choice(Weapons)
     print('Your weapon is ' + weapon + '\n')
 
@@ -76,3 +81,6 @@ while RetryGame == 'Yes'.lower():
     PrintSleep('You ' + random.choice(Game_Ending) + '!', 3)
     print('You Finished Your Adventure!')
     RetryGame = input('Do you want to play again?\n')
+    while RetryGame not in Options:
+        print('Please enter a valid response (Yes/Y or No/N.)')
+        RetryGame = input('Do you want to play again?\n')
